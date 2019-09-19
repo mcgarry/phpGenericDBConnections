2116b829fcfecedf192e8b5a1fd5e669cad74138
Seán Mc Garry <mcgarry@users.noreply.github.com>
2019-09-19 16:47:07 +0100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,43 @@
+<?php
+
+$pdo_host = 'localhost';
+$pdo_user = 'root';
+$pdo_pass = '';
+// replace '???_db' with your database name 
+$pdo_database = '???_db';
+	
+function connData($dataSQL,$params,$isShowEcho,$isReturned){
+	global $pdo_host, $pdo_user, $pdo_pass, $pdo_database;
+	try {
+	   $pdo_dns = 'mysql:host='.$pdo_host.';dbname='.$pdo_database;
+		$pdo_db = new PDO($pdo_dns, $pdo_user, $pdo_pass);
+		$pdo_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$pdo_sql = $pdo_db->prepare($dataSQL);
+		$pdo_sql->execute($params);
+		if($isReturned){
+			return $pdo_sql->fetchAll(PDO::FETCH_ASSOC);
+		}
+		$pdo_db = null; 
+		if($isShowEcho){
+			echo "Connected successfully";
+		}
+	}
+	catch(PDOException $e)
+	{
+    	if($isShowEcho){
+			echo "Connection failed: " . $e->getMessage();
+		}
+    }
+}
+
+
+function getData($dataSQL,$params,$isShowEcho){
+	return = connData($dataSQL,$params,$isShowEcho,true);
+}
+
+function setData($dataSQL,$params,$isShowEcho){
+	connData($dataSQL,$params,$isShowEcho,false);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+	// include connection function getData()
+	//include 'conn.php';
+	
+	// PDO SQL
+	//$sql = "SELECT * FROM `???_tbl`";
+	
+	// PDO Params
+	//$params = array();
+	
+	// Put results into $data
+	//$data = getData($sql,$params);
+	
+	// uncomment to see results
+	//var_dump($data);
+	//echo ""---------------------------;
+	//var_dump($data[0]);
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+
+</body>
+</html>
